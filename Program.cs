1b035ccc5482e19b53d32e059b5bdada1c612e95
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-08-03 23:17:37 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class viewclock
+{
+    static int x;static int y;static String s = "";
+    public static void Views() 
+    {
+        DateTime now = DateTime.Now;
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine(s+now.ToString());
+    
+    
+    }
+    public static void clockLoop(int xx, int yy, String ss)
+    { 
+        x= xx; y = yy; s = ss;
+        while (true)
+        {
+            Views();
+            System.Threading.Thread.Sleep(1000);
+            if (Console.KeyAvailable) break;
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+class clocks
+{
+
+    public static void Main()
+    {
+
+        Console.BackgroundColor= ConsoleColor.White;
+        Console.ForegroundColor= ConsoleColor.Black;
+        Console.Clear();
+        viewclock.clockLoop(Console.WindowWidth / 2 - 10, Console.WindowHeight / 2, ":");
+
+
+
+    }
+
+
+
+
+
+
+}
+
